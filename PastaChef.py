00318bae33a6b00cf8b6722057fc77c9f3f70e3a
TinasheMadangur <62337915+tinashem-hub@users.noreply.github.com>
2021-10-20 07:46:14 +0000

    Initial commit

---
+++ PastaChef.py
@@ -0,0 +1,7 @@
+#Creating my first orders
+
+name = input("Hello welcome to PastaChef , what's your name \n")
+
+order =input("Please " + name + " select from our menu : pizza , maccorroni , lasgne \n" ) 
+
+print("Okay " + name + " your " + order + " will be ready soon 😊" )
